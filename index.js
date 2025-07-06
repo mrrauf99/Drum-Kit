99348ee6fcd77fdcde10e09fea9018fc088e6932
Abdul Rauf <itxrauf99@gmail.com>
2025-07-06 15:22:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var buttons = document.getElementsByTagName("button");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", HandleClick);
+}
+// it attaches Event Listener to all buttons when page loads
+
+document.addEventListener("keydown", HandleKey);
+
+function HandleClick() {
+  PlaySound(this.textContent);
+}
+
+function HandleKey(event) {
+  PlaySound(event.key.toLowerCase());
+}
+
+function PlaySound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+      break;
+  }
+}
