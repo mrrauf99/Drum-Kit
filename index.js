c0dc10744c08ac6859b4be35ddc9e75bcd33ada0
Abdul Rauf <itxrauf99@gmail.com>
2025-08-26 00:14:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const sounds = {
   l: new Audio("sounds/kick-bass.mp3"),
 };
 
+let unlockedAudio = false;
+
 // Handle both mouse and touch
 $("button").on("pointerdown", function () {
   const key = $(this).text().toLowerCase();
@@ -25,8 +27,15 @@ $(document).on("keydown", function (event) {
 // Play sound safely
 function PlaySound(key) {
   if (sounds[key]) {
-    sounds[key].currentTime = 0;
-    sounds[key].play();
+    if (!unlockedAudio) {
+      setTimeout(() => {
+        sounds[key].play();
+        unlockedAudio = true;
+      }, 100);
+    } else {
+      sounds[key].currentTime = 0;
+      sounds[key].play();
+    }
   }
 }
 
